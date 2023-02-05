5cfb2b315fb890419b4c9bf744603c2195ce06fb
KyzmenkoDiana <kyzmenko.d.1.21@gmail.com>
2023-02-05 19:12:27 +0300

    12

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,18 +2,27 @@
 export default {
     data() {
         return {
-            num1: 48,
-            num2: 25,
+            date: '3'
         }
     },
     methods: {
-        show: function (str) {
-            return str[0].toUpperCase() + str.slice(1);
-        }
+        show: function () {
+            let date = this.dayOfWeek(this.date);
+            alert(date);
+        },
+        dayOfWeek: function (date) {
+            let days = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
+            return days[date];
+        },
+        main: function () {
+            const dayOfWeekDigit = new Date().getDay();
+            let days = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
+            return dayOfWeekDigit[days]
+        },
     }
 }
 </script>
 <template>
-    <div>{{ num1 + num2 }}</div>
+    <button v-on:mouseover="show">Мышкой </button>
 </template>
 
