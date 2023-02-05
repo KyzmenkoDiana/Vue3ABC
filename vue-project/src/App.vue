6644953c931df9d05eadbdf229a674cf58d48752
KyzmenkoDiana <kyzmenko.d.1.21@gmail.com>
2023-02-05 20:59:17 +0300

    18

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,8 +2,7 @@
 export default {
     data() {
         return {
-            visible1: true,
-            visible2: false,
+            hidden1: false,
         }
     }
 }
@@ -11,8 +10,6 @@ export default {
 </script>
 
 <template>
-    <p v-if="visible1">Hello my name Diana</p>
-    <h1></h1>
-    <p v-if="visible2">I life in Armyansk</p>
+    <p v-if="!hidden1">Hello my name is Diana</p>
 </template>
 
