71500e4b9c7874c3d3d6ab195eb2b537169fd9e9
KyzmenkoDiana <kyzmenko.d.1.21@gmail.com>
2023-02-05 21:08:16 +0300

    19

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -4,12 +4,22 @@ export default {
         return {
             hidden1: false,
         }
+    },
+    methods: {
+        hide: function () {
+            this.hidden1 = true;
+        },
+        hide2: function () {
+            this.hidden1 = false;
+        }
     }
 }
 
 </script>
 
 <template>
-    <p v-if="!hidden1">Hello my name is Diana</p>
+    <p v-if="hidden1">Hello my name is Diana</p>
+    <button @click="hide">Нажми</button>
+    <button @click="hide2">Упсс</button>
 </template>
 
