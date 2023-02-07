082e5e11270d3ea7d54122daae3cd64f399f37b4
KyzmenkoDiana <kyzmenko.d.1.21@gmail.com>
2023-02-07 17:59:26 +0300

    29

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,19 +2,29 @@
 export default {
     data() {
         return {
-            arr: ['x', 'y', 'z'],
+            Diana: { user1: '100$', user2: '200$', user3: '300$' },
         }
     }
 }
 </script>
 
 <template>
-    <ul v-for="(elem, key) in arr">
-        {{ key }} {{ elem }} </ul>
+    <ul>
+        <li v-for="elem in Diana">{{ elem }}</li>
+    </ul>
+    <b>
+    </b>
+    <ul>
+        <li v-for="(key, elem) in Diana"> {{ elem }} - {{ key }}</li>
+    </ul>
+    <b>
+    </b>
+    <ul>
+        <li v-for="(key, elem, index) in Diana"> {{ elem }} - {{ key }} - {{ index }}</li>
+    </ul>
+    <b>
+    </b>
+    <ul>
+        <li v-for="(key, elem, index) in Diana"> {{ elem }} - {{ key }} - {{ index+ 1 }}</li>
+    </ul>
 </template>
-<style>
-.diana {
-    color: blue;
-    font-size: 20px;
-}
-</style>
