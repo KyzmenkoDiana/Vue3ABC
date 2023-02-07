82e3a38ec0f5e2a3bffc8cc8d024e990207e27bd
KyzmenkoDiana <kyzmenko.d.1.21@gmail.com>
2023-02-07 20:14:25 +0300

    39

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,26 +2,49 @@
 export default {
     data() {
         return {
-            obj: {
-                done: false,
-                selected: true,
+            Dina: true,
+            diana: true,
+            style: {
+                hidden: true,
             }
+        }
+    },
+    methods: {
+        setDone: function () {
+            this.style.hidden = false;
+        },
+        setTwo: function () {
+            this.style.hidden = true;
+        },
+        Diana: function () {
+            this.Dina = !this.Dina;
         }
     }
 }
 </script>
 
 <template>
-    <p :class="obj">Avocado</p>
+    <button @click="Diana">Avocado</button>
+    <p class="Avocado" v-if="Dina">Avocado</p>
+    <p :class="style">Avocado</p>
+    <button @click="setDone">показывает</button>
+    <button @click="setTwo">прячет</button>
+
 </template>
+
+
 <style>
-.done {
+p {
+    color: blue;
+    font-size: 20px;
+}
+
+.Avocado {
     color: red;
     font-size: 20px;
 }
 
-.selected {
-    color: blue;
-    font-size: 20px;
+p.hidden {
+    display: none;
 }
 </style>
