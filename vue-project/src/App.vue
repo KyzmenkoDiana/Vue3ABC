c6478b56a1593759c50ff52f84a47da685bd516a
KyzmenkoDiana <kyzmenko.d.1.21@gmail.com>
2023-02-07 18:31:40 +0300

    32

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,46 +2,16 @@
 export default {
     data() {
         return {
-            hrefs: [
-                { href: '1.html', text: 'diana1' },
-                { href: '2.html', text: 'diana2' },
-                { href: '3.html', text: 'diana3' },
-            ],
-            products: [
-                {
-                    name: 'Djulia',
-                    price: 100,
-                    quantity: 5
-                },
-                {
-                    name: 'Vector',
-                    price: 200,
-                    quantity: 4
-                },
-                {
-                    name: 'Flaffi',
-                    price: 300,
-                    quantity: 3
-                },
-            ]
+
         }
     }
 }
 </script>
 
 <template>
-    <ul>
-        <li v-for="link in hrefs"><a class="diana" hrefs="{{ link.href }}">{{ link.text }}</a></li>
-    </ul>
-    <b>
-    </b>
-    <table>
-        <tr class="diana2" v-for="col in products">
-            <td>{{ col.name }}</td>
-            <td>{{ col.price }}</td>
-            <td>{{ col.quantity }}</td>
-        </tr>
-    </table>
+    <p v-for="num in 30">
+        {{ num }}
+    </p>
 </template>
 <style>
 .diana {
