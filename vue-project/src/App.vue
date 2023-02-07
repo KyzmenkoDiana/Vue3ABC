923e394f464ffd5c0c8a03b832903bca79425a2f
KyzmenkoDiana <kyzmenko.d.1.21@gmail.com>
2023-02-07 17:33:36 +0300

    26

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,38 +2,29 @@
 export default {
     data() {
         return {
-            diana: true,
+            Avocado: true,
         }
     },
     methods: {
         Diana: function () {
-            this.diana = false;
+            this.Avocado = false;
         }
     }
 }
 </script>
 
 <template>
-    <button @click="Diana">diana</button>
-    <div v-if="diana">
-        <p class="diana">Djulia</p>
-        <p class="diana1">Vector</p>
-        <p class="diana2">Flaffi</p>
-    </div>
+    <!--1-->
+    <button @click="Diana">Diana</button>
+    <p class="Avocado" v-show="Avocado">Djulia</p>
+    <!--
+        2 - v-show будет всегда отрисовываться и оставаться в DOM, a v-if выполняет настоящую условную отрисовку.
+        3 - v-if если условие ложно на момент первоначальной отрисовки, v-show элемент всегда отрисовывается
+        4 - директиву v-show нельзя использовать на элементе template и она не работает с v-else.-->
 </template>
 <style>
-.diana {
+.Avocado {
     color: blue;
-    font-size: 30px;
-}
-
-.diana1 {
-    color: red;
-    font-size: 30px;
-}
-
-.diana2 {
-    color: yellow;
     font-size: 30px;
 }
 </style>
