6b069c2e10ec6527c298baea6d3003e8499b49d8
KyzmenkoDiana <kyzmenko.d.1.21@gmail.com>
2023-02-07 19:02:54 +0300

    36

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,55 +2,23 @@
 export default {
     data() {
         return {
-            Avocado: ['Djulia', 'vector', 'Flaffi'],
         }
-    },
-    methods: {
-        d: function () {
-            this.Avocado.push('Krah');
-        },
-        i: function () {
-            this.Avocado.shift();
-        },
-        a: function () {
-            this.Avocado.pop();
-        },
-        n: function () {
-            this.Avocado.splice(1, 4);
-        },
-        b: function () {
-            this.Avocado.sort();
-        },
-        g: function () {
-            this.Avocado.reverse();
-        },
     }
 }
 </script>
 
 <template>
-    <ul>
-        <li class="diana" v-for="elem in Avocado">{{ elem }}</li>
-    </ul>
-    <button class="diana2" @click="add">1</button>
-    <br>
-    <button class="diana2" @click="d">2</button>
-    <br>
-    <button class="diana2" @click="a">3</button>
-    <br>
-    <button class="diana2" @click="n">4</button>
-    <br>
-    <button class="diana2" @click="b">5</button>
-    <br>
-    <button class="diana2" @click="g">6</button>
+    <p>Hello my name Diana</p>
+    <p></p>
+    <p1>I life in Armyansk</p1>
 </template>
 <style>
-.diana {
+p {
     color: red;
     font-size: 20px;
 }
 
-.diana2 {
+p1 {
     color: blue;
     font-size: 20px;
 }
