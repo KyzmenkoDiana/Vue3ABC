cb817009c12247daa4431d2cb1dbc089fa8d46bd
KyzmenkoDiana <kyzmenko.d.1.21@gmail.com>
2023-02-07 18:28:07 +0300

    31

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,24 +2,54 @@
 export default {
     data() {
         return {
-            diana: [1, 2, 3],
+            hrefs: [
+                { href: '1.html', text: 'diana1' },
+                { href: '2.html', text: 'diana2' },
+                { href: '3.html', text: 'diana3' },
+            ],
+            products: [
+                {
+                    name: 'Djulia',
+                    price: 100,
+                    quantity: 5
+                },
+                {
+                    name: 'Vector',
+                    price: 200,
+                    quantity: 4
+                },
+                {
+                    name: 'Flaffi',
+                    price: 300,
+                    quantity: 3
+                },
+            ]
         }
     }
 }
 </script>
 
 <template>
-    <div v-for="elem in diana">
-        <p class="divider">{{ elem }}</p>
-    </div>
+    <ul>
+        <li v-for="link in hrefs"><a class="diana" hrefs="{{ link.href }}">{{ link.text }}</a></li>
+    </ul>
     <b>
     </b>
-    <ul v-for="elem in diana">
-        <li class="divider">{{ elem }}</li>
-    </ul>
+    <table>
+        <tr class="diana2" v-for="col in products">
+            <td>{{ col.name }}</td>
+            <td>{{ col.price }}</td>
+            <td>{{ col.quantity }}</td>
+        </tr>
+    </table>
 </template>
 <style>
-.divider {
+.diana {
+    color: red;
+    font-size: 20px;
+}
+
+.diana2 {
     color: blue;
     font-size: 20px;
 }
