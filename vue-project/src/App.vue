31f3d3436d258714b22799bfbfc1937a584c9722
KyzmenkoDiana <kyzmenko.d.1.21@gmail.com>
2023-02-08 16:02:43 +0300

    47

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,19 +2,37 @@
 export default {
     data() {
         return {
-            text: 'Hello Diana',
+            arr: [],
         }
     }
 }
 </script>
 
 <template>
-    <textarea v-model="text"></textarea>
-    <p class="diana">{{ text }}</p>
+    <div class="diana">
+        <input class="diana1" type="checkbox" v-model="arr" value="English">
+        <label>English</label>
+        <br>
+        <input class="diana1" type="checkbox" v-model="
+        arr" value="Turkish">
+        <label>Turkish</label>
+        <br>
+        <input class="diana1" type="checkbox" v-model="arr" value="Ukrainian">
+        <label>Ukrainian</label>
+        <ol>
+            <ul v-for="elem in arr">{{ elem }}</ul>
+        </ol>
+    </div>
+    <br>
 </template>
 <style>
 .diana {
     color: blue;
+    font-size: 20px;
+}
+
+.diana1 {
+    color: red;
     font-size: 20px;
 }
 </style>
