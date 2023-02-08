aac5d6ca3547cdb5779e588e8a1da8137a41d1fd
KyzmenkoDiana <kyzmenko.d.1.21@gmail.com>
2023-02-08 19:48:01 +0300

    57

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,65 +2,17 @@
 export default {
     data() {
         return {
-            users: [
-                {
-                    id: 1,
-                    name: 'Vector',
-                    salary: 129,
-                    age: 38,
-                },
-                {
-                    id: 2,
-                    name: 'Djuliet',
-                    salary: 250,
-                    age: 91,
-                },
-                {
-                    id: 3,
-                    name: 'Flaffi',
-                    salary: 328,
-                    age: 61,
-                },
-            ]
         }
-    },
-    methods: {
-        edit(user) {
-            user.isEdit = true;
-        },
-        save(user) {
-            user.isEdit = false;
-        },
     }
 }
 </script>
 
 <template>
-    <table class="diana">
-        <tr v-for="user in users" :key="user.id">
-            <template v-if="!user.isEdit">
-                {{ user.name }}
-                {{ user.salary }}
-                {{ user.age }}
-                <button class="button" @click="edit(user)">edit</button>
-            </template>
-            <template v-else>
-                <input v-model="user.name">
-                <input v-model="user.salary">
-                <input v-model="user.age">
-                <button class="button" @click="save(user)">
-                    save
-                </button>
-            </template>
-        </tr>
-    </table>
+    <p class="diana">В этом задании был только текст про компоненты</p>
 </template>
 <style>
 .diana {
     color: blue;
-}
-
-.diana1 {
-    color: red;
+    font-size: 20px;
 }
 </style>
