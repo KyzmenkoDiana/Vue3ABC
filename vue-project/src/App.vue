5918e6019c7034c44549d9bf8d21ad66560e397f
KyzmenkoDiana <kyzmenko.d.1.21@gmail.com>
2023-02-08 19:10:30 +0300

    55

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,28 +1,49 @@
 <script>
-import { enableTracking } from '@vue/reactivity';
-
 export default {
     data() {
         return {
-            diana: ['Vector', 'Flaffi', 'Djuliet', 'Diana', 'Polina'],
-
-        }
-    },
-    methods: {
-        Diana: function (index) {
-            this.diana.splice(index, 1);
-        }
+            users: [
+                {
+                    id: 1,
+                    name: 'vector',
+                    salary: 100,
+                    age: 30,
+                },
+                {
+                    id: 2,
+                    name: 'Flaffi',
+                    salary: 200,
+                    age: 40,
+                },
+                {
+                    id: 3,
+                    name: 'Djuliya',
+                    salary: 300,
+                    age: 50,
+                },
+                methods: {
+                    Diana: function (id) {
+                        this.users = this.users.filter((user) => {
+                            return user.id !== id;
+                        })
+                    }
+                }
+    }
     }
 }
 </script>
 
 <template>
-    <ul>
-        <li class="diana" v-for="(item, index) in diana" :key="index">
-            {{ item }}
-            <button @click="Diana(index)">remove</button>
-        </li>
-    </ul>
+    <table class="diana">
+        <ul>
+            <li v-for="user in users" :key="user.id">
+                {{ user.name }}
+                {{ user.salary }}
+                {{ user.age }}
+                <button @click="Diana(user.id)">delete</button>
+            </li>
+        </ul>
+    </table>
 </template>
 <style>
 .diana {
