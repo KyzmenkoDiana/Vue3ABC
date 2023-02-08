4db3610023aa21dd223626df731c56cf7cc8fef5
KyzmenkoDiana <kyzmenko.d.1.21@gmail.com>
2023-02-08 16:45:14 +0300

    49

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,49 +2,31 @@
 export default {
     data() {
         return {
-            language: '',
-            d: false,
-            di: false,
-            diana: false
-        }
-    },
-    methods: {
-        Djuliet() {
-            this.d = true;
-        },
-        Vector() {
-            this.di = true;
-        },
-        Flaffi() {
-            this.diana = true;
+            selected: 'Where do you live',
         }
     }
 }
 </script>
 
 <template>
-    <input type="radio" v-model="language" value="Turkish" @click="Djuliet">
-    <p class="diana">Turkish</p>
-    <p></p>
-    <input type="radio" v-model="language" value="Ukrainian" @click="Vector">
-    <p class="diana">Ukrainian</p>
-    <p></p>
-    <input type="radio" v-model="language" value="Russian" @click="Flaffi">
-    <p></p>
-    <p class="diana">Russian</p>
-    <p class="diana1" v-if="d">Mahraba, benim adim Diana</p>
-    <p class="diana1" v-else-if="di">Привiт, мене звуть Дiана</p>
-    <p class="diana1" v-else-if="diana">Привет, меня зовут Диана</p>
+    <select v-model="selected">
+        <option class="diana">Turkish</option>
+        <option class="diana1">Russian</option>
+        <option class="diana2">USA</option>
+    </select>
 
+    <p>{{ selected }}</p>
 </template>
 <style>
 .diana {
     color: blue;
-    font-size: 20px;
 }
 
 .diana1 {
     color: red;
-    font-size: 20px;
+}
+
+.diana2 {
+    color: green;
 }
 </style>
