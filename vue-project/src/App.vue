79b14010531621a3252e1c8037efde1212c75c63
KyzmenkoDiana <kyzmenko.d.1.21@gmail.com>
2023-02-08 15:52:20 +0300

    45

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,34 +2,19 @@
 export default {
     data() {
         return {
-            checked: true,
-        }
-    },
-    methods: {
-        toggle: function () {
-            this.checked = false;
+            text: 'Hello Diana',
         }
     }
 }
 </script>
 
 <template>
-    <input class="diana" type="checkbox" v-model="checked">
-    <p class="diana1" v-if="checked">{{ checked? 'Hello Diana': 'no' }}</p>
+    <textarea v-model="text"></textarea>
+    <p class="diana">{{ text }}</p>
 </template>
 <style>
 .diana {
     color: blue;
-    font-size: 20px;
-}
-
-.diana1 {
-    color: red;
-    font-size: 20px;
-}
-
-.diana2 {
-    color: green;
     font-size: 20px;
 }
 </style>
